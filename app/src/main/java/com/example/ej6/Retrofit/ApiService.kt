324d49fb1b370260725ce17a7c1a3c0f8b589deb
Emilio Palma <l21170217@culiacan.tecnm.mx>
2024-12-03 03:10:55 -0700

    Consumo de una API

--- app/src/main/java/com/example/ej6/Retrofit/ApiService.kt
+++ app/src/main/java/com/example/ej6/Retrofit/ApiService.kt
@@ -1,16 +1,17 @@
 package com.example.ej6.models.Retrofit
 
+import com.example.ej6.models.Book
 import retrofit2.http.*
 
 interface ApiService {
     @GET("api/Books")
-    suspend fun getBooks(): List<com.example.ej6.models.Book>
+    suspend fun getBooks(): List<Book>
 
     @POST("api/Books")
-    suspend fun addBook(@Body book: com.example.ej6.models.Book): com.example.ej6.models.Book
+    suspend fun addBook(@Body book: Book): Book
 
     @PUT("api/Books/{id}")
-    suspend fun updateBook(@Path("id") id: Int, @Body book: com.example.ej6.models.Book): com.example.ej6.models.Book
+    suspend fun updateBook(@Path("id") id: Int, @Body book: Book): Book
 
     @DELETE("api/Books/{id}")
     suspend fun deleteBook(@Path("id") id: Int)
